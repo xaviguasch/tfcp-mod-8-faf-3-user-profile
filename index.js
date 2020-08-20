651f3b999e2caa1380161f818bca5333da27ebbd
xaviguasch <xaviguasch@gmail.com>
2020-08-20 19:06:06 +0200

    added starter files

---
+++ index.js
@@ -0,0 +1,13 @@
+/* 
+    Create a User Profile using data from
+        https://jsonplaceholder.typicode.com/users/3
+    
+    The User Profile must be a Flexbox container with 4 components 
+        1. Profile Header
+            - With the User's name and username
+        2. Company
+            - Displaying information about their company
+        3. Contact Details
+            - Contains Email/Phone/Website
+        4. User Address
+*/
