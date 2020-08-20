b5561989adc71ec0aebbda244f3c89d614d79c5d
xaviguasch <xaviguasch@gmail.com>
2020-08-20 19:49:01 +0200

    added first solution

--- index.js
+++ index.js
@@ -11,3 +11,35 @@
             - Contains Email/Phone/Website
         4. User Address
 */
+
+// getemoji.com
+
+async function getUser() {
+  let response = await fetch('https://jsonplaceholder.typicode.com/users/3')
+  let user = await response.json()
+  return user
+}
+
+getUser().then((user) => {
+  console.log(user)
+  document.body.innerHTML = `<div class="user-profile">
+      <div class="user-profile-header">
+          <div>${user.name}</div>
+          <div>@${user.username}</div>
+      </div>
+      <div class="user-profile-company">
+          <div>👩🏽‍💼${user.company.name}</div>
+          <div>${user.company.catchPhrase}</div>
+          <div>${user.company.bs}</div>
+      </div>
+      <div class="user-profile-contact">
+          <div>📧${user.email}</div>
+          <div>📞${user.phone}</div>
+          <div>💻${user.website}</div>
+      </div>
+      <div class="user-profile-address">
+          <div>${user.address.street}, ${user.address.suite}</div>
+          <div>${user.address.city} ${user.address.zipcode}</div>
+      </div>
+  </div>`
+})
